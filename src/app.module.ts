e4b96883c3563cc1970e51f4dee38c0d6e814617
denzel <denzelvojn@gmail.com>
2025-07-23 21:37:27 +0300

    конфиг бд

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,26 +1,24 @@
-import { Module } from '@nestjs/common';
-import { ConfigModule, ConfigService } from '@nestjs/config';
-import { SequelizeModule } from '@nestjs/sequelize';
+import {Module} from '@nestjs/common';
+import {ConfigModule} from '@nestjs/config';
+import {SequelizeModule} from '@nestjs/sequelize';
+import * as process from "node:process";
 
 @Module({
     imports: [
         ConfigModule.forRoot({
             isGlobal: true,
         }),
-        SequelizeModule.forRootAsync({
-            imports: [ConfigModule],
-            inject: [ConfigService],
-            useFactory: (configService: ConfigService) => ({
-                dialect: 'postgres',
-                host: 'localhost',
-                port: 5432,
-                username: configService.get<string>('DB_USERNAME'),
-                password: configService.get<string>('DB_PASSWORD'),
-                database: 'potato',
-                models: [],
-                autoLoadModels: true,
-            }),
+        SequelizeModule.forRoot({
+            dialect: 'postgres',
+            host: 'localhost',
+            port: 5432,
+            username: process.env.DB_USERNAME,
+            password: process.env.DB_PASSWORD,
+            database: 'potato',
+            models: [],
+            autoLoadModels: true,
         }),
     ],
 })
-export class AppModule {}
+export class AppModule {
+}
